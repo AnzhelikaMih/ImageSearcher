d9959379d9437a69a7a0217209d3d0de70dba176
Anzhelika Mihalchenia <anzhelichka1990@gmail.com>
2024-09-12 11:13:47 +0300

    Created Detailed ViewController

--- ImageSearch/Detailed screen/DetailedViewController.swift
+++ ImageSearch/Detailed screen/DetailedViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import Photos
 
 protocol DetailViewControllerProtocol {
     
 }
 
 class DetailViewController: UIViewController {
+    // MARK: - Properties
     var photo: Image
     
+    // MARK: - Components
+    private lazy var imageView: UIImageView = {
+        let image = UIImageView()
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 15
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Constants.Colors.mainTextColor
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var authorLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Constants.Colors.mainTextColor
+        label.font = UIFont.italicSystemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Save to Gallery".uppercased(), for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(saveImageToGallery), for: .touchUpInside)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - Initialization
     init(photo: Image) {
         self.photo = photo
         super.init(nibName: nil, bundle: nil)
@@ -23,9 +63,91 @@ class DetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Constants.Colors.backgroungColor
+        setupView()
+        configureView()
     }
 
+    // MARK: - Private functions
+    private func setupView() {
+        view.addSubview(imageView)
+        view.addSubview(descriptionLabel)
+        view.addSubview(authorLabel)
+        view.addSubview(saveButton)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        authorLabel.translatesAutoresizingMaskIntoConstraints = false
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1.0),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            authorLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8),
+            authorLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            authorLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            saveButton.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            saveButton.heightAnchor.constraint(equalToConstant: 42)
+        ])
+    }
+    
+    private func configureView() {
+        if let imageUrl = photo.urls?.regular, let url = URL(string: imageUrl) {
+            loadImage(from: url)
+        }
+        descriptionLabel.text = photo.altDescription ?? "No description available."
+        authorLabel.text = "Author: \(photo.user?.name ?? "Unknown")"
+    }
+    
+    private func loadImage(from url: URL) {
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            }
+        }
+    }
+    
+    @objc private func saveImageToGallery() {
+        guard let image = imageView.image else { return }
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .authorized {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+            } else {
+                let attribute = Alert.Attribute(
+                    message: "Permission denied to save images."
+                )
+                self.presentAlert(with: attribute)
+            }
+        }
+    }
+    
+    @objc private func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if error != nil {
+            let attribute = Alert.Attribute(
+                message: "Failed to save image."
+            )
+            presentAlert(with: attribute)
+        } else {
+            let attribute = Alert.Attribute(
+                message: "Image saved to gallery successfully."
+            )
+            presentAlert(with: attribute)
+        }
+    }
 }
